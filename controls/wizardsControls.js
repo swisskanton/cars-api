494ab571eac96bf6fb4561545ef79572fada6ed0
swisskanton <kormanyos.antal@gmail.com>
2025-01-28 23:01:22 +0100

    delete response changed

--- controls/wizardsControls.js
+++ controls/wizardsControls.js
@@ -27,12 +27,14 @@ export const createWizard = async (req, res) => {
 		if (!name || !house || !boggart) {
 			return res.status(400).json({ message: "Missing data" });
 		}
-		const wizard = await Wizard.findOne({ where: { name, house, boggart }});
+		const wizard = await Wizard.findOne({
+			where: { name, house, boggart },
+		});
 		if (wizard) {
 			return res.status(409).json({ message: "Wizard already exists" });
 		}
-    const newWizard = await Wizard.create({ name, house, boggart });
-    if (!newWizard) {
+		const newWizard = await Wizard.create({ name, house, boggart });
+		if (!newWizard) {
 			return res.status(500).json({ message: "Database error" });
 		}
 		res.status(200).json(newWizard.toJSON());
@@ -42,8 +44,8 @@ export const createWizard = async (req, res) => {
 };
 
 export const updateWizard = async (req, res) => {
-  try {
-    if (req.params.id <= 6) {
+	try {
+		if (req.params.id <= 6) {
 			return res.status(405).json({ message: "Protected base data" });
 		}
 		const wizard = await Wizard.findByPk(req.params.id);
@@ -55,7 +57,7 @@ export const updateWizard = async (req, res) => {
 			return res.status(400).json({ message: "Missing data" });
 		}
 		const updatedWizard = await wizard.update({ name, house, boggart });
-    if (!updatedWizard) {
+		if (!updatedWizard) {
 			return res.status(500).json({ message: "Database error" });
 		}
 		res.status(200).json(updatedWizard.toJSON());
@@ -77,7 +79,7 @@ export const deleteWizard = async (req, res) => {
 		if (!result) {
 			return res.status(500).json({ message: "Database error" });
 		}
-		res.status(200).json(wizards);
+		res.status(200).json({ message: "Delete successful" });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
